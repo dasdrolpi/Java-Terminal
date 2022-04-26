7b5597de330a890a6380b8274091c89c79623260
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-26 21:48:29 +0200

    client: Prevent the creation of instances of the Main class

--- client/src/main/java/de/drolpi/terminal/client/Main.java
+++ client/src/main/java/de/drolpi/terminal/client/Main.java
@@ -5,6 +5,10 @@ import de.natrox.console.jline3.JLine3Console;
 
 public class Main {
 
+    private Main() {
+        throw new UnsupportedOperationException();
+    }
+
     public static void main(String[] args) throws Exception {
         Console console = JLine3Console
             .builder()
