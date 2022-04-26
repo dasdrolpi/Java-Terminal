63002edfdaffdc0d78db31a7e986a9cbb69ed346
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-26 21:17:16 +0200

    Add Main classes

---
+++ client/src/main/java/de/drolpi/terminal/client/Main.java
@@ -0,0 +1,9 @@
+package de.drolpi.terminal.client;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ server/src/main/java/de/drolpi/terminal/server/Main.java
@@ -0,0 +1,9 @@
+package de.drolpi.terminal.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
