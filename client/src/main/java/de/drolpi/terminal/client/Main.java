5649e53cc3458ad99ef9cfdd0967eb748e48c6cf
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-26 21:43:52 +0200

    client: Build Console

--- client/src/main/java/de/drolpi/terminal/client/Main.java
+++ client/src/main/java/de/drolpi/terminal/client/Main.java
@@ -1,9 +1,15 @@
 package de.drolpi.terminal.client;
 
+import de.natrox.console.Console;
+import de.natrox.console.jline3.JLine3Console;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Console console = JLine3Console
+            .builder()
+            .prompt(() -> "> ")
+            .build();
     }
 
 }
