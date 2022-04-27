4ad61d2965f0179f2de9b32cfbe3dad4ee592ea9
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-27 15:52:12 +0200

    Add launcher module

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -13,10 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-rootProject.name = "Terminal"
 
-include(
-    ":server",
-    ":client",
-    ":launcher"
-)
